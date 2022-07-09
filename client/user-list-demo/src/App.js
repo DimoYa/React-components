93d0d20abbf5c82d7f02502772477d29bbc73488
Dimo Yanev <Dimo_Yanev@abv.bg>
2022-07-10 02:10:03 +0300

    implement get all users and get by id

--- client/user-list-demo/src/App.js
+++ client/user-list-demo/src/App.js
@@ -1,10 +1,22 @@
+import { useEffect, useState } from 'react';
+
+import * as userService from './services/userService';
+
 import { Header } from './components/common/Header';
 import { Footer } from './components/common/Footer';
 import { Search } from './components/search/Search';
-import { UserSection } from './components/user-section/UserSection';
+import { UserList } from './components/user-list/UserList';
 import './App.css';
 
 function App() {
+  const [users, setUsers] = useState([]);
+
+  useEffect(() => {
+    userService.getAll().then((users) => setUsers(users));
+  }, []);
+
+  console.log(users);
+
   return (
     <div>
       <Header />
@@ -12,7 +24,7 @@ function App() {
       <main className="main">
         <section className="card users-container">
           <Search />
-          <UserSection />
+          <UserList users={users} />
         </section>
       </main>
 
