1cf27fa3e4cea2caf1c4173e6cb1d23c123be013
Dimo Yanev <Dimo_Yanev@abv.bg>
2022-07-10 00:05:23 +0300

    add componnets about headr, footer, user-list

--- client/user-list-demo/src/App.js
+++ client/user-list-demo/src/App.js
@@ -1,9 +1,22 @@
+import { Header } from './components/common/Header';
+import { Footer } from './components/common/Footer';
+import { Search } from './components/search/Search';
+import { UserSection } from './components/user-section/UserSection';
 import './App.css';
 
 function App() {
   return (
     <div>
-      <h1>users</h1>
+      <Header />
+
+      <main className="main">
+        <section className="card users-container">
+          <Search />
+          <UserSection />
+        </section>
+      </main>
+
+      <Footer />
     </div>
   );
 }
---
+++ client/user-list-demo/src/components/common/Footer.js
@@ -0,0 +1,7 @@
+export const Footer = () => {
+  return (
+    <footer className="footer">
+      <p>Copyright © designed by Dimo Yanev</p>
+    </footer>
+  );
+};
